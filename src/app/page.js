413db317374ae1c23d8fc9f2187f947c7c8c0fab
HridoyHoque <hhridoy155@gmail.com>
2023-08-10 01:58:52 +0600

    Added Mini profile

---
+++ src/app/page.js
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+import MiniProfile from './components/MiniProfile'
+
+
+export default function Home() {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 md:max-w-3xl xl:grid-cols-3 xl:max-w-6xl mx-auto'>
+      <section className='col-span-2'>
+        <div></div>
+      </section>
+      <section className='hidden xl:inline-grid md:col-span-1'>
+       <div className='fixed top-20'>
+       <MiniProfile />
+       </div>
+      </section>
+    </div>
+  )
+}
